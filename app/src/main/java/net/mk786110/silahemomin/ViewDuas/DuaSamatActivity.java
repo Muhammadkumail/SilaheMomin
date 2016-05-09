869cbea1ed6f08d37a67a17f2895f9d84943ba0c
Muhammad kumail <muhammad.kumail449@gmail.com>
2016-05-09 23:43:31 +0500

    Gcm Registertion issue solved

--- app/src/main/java/net/mk786110/silahemomin/ViewDuas/DuaSamatActivity.java
+++ app/src/main/java/net/mk786110/silahemomin/ViewDuas/DuaSamatActivity.java
@@ -2,6 +2,7 @@ package net.mk786110.silahemomin.ViewDuas;
 
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -34,7 +35,13 @@ public class DuaSamatActivity extends AppCompatActivity {
         new get_data_AsynchTask().execute();
 
     }
-
+    DialogInterface.OnCancelListener cancelListener=new DialogInterface.OnCancelListener(){
+        @Override
+        public void onCancel(DialogInterface arg0){
+            bCancelled=true;
+            finish();
+        }
+    };
 
     private class get_data_AsynchTask extends AsyncTask<Void, Void, Void> {
         ProgressDialog progressDialog;
@@ -43,6 +50,9 @@ public class DuaSamatActivity extends AppCompatActivity {
         @Override
         protected void onPreExecute() {
             progressDialog= ProgressDialog.show(DuaSamatActivity.this, "wait", C.Salwat, true);
+            progressDialog.setCancelable(true);
+            progressDialog.setOnCancelListener(cancelListener);
+            bCancelled=false;
             arrayList = new ArrayList<>();
             mDuaSamatDataSource = new DuaSamatDataSource(context);
             super.onPreExecute();
